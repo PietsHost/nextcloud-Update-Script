10c8aeb48e5a0e1e1e81d9ce7891d90cce1d74d5
PietsHost <info@piets-host.de>
2017-04-17 00:50:49 +0200

    little fixes

--- ncupdate.sh
+++ ncupdate.sh
@@ -9,61 +9,74 @@
 # https://www.techandme.se/nextcloud-vm
 # https://github.com/nextcloud/vm/blob/master/nextcloud_update.sh
 
-
-echo -e "\e[32m ______ __         __           _______               __   
-|   __ \__|.-----.|  |_.-----. |   |   |.-----.-----.|  |_ 
-|    __/  ||  -__||   _|__ --| |       ||  _  |__ --||   _|
-|___|  |__||_____||____|_____| |___|___||_____|_____||____|\e[0m"
-echo ""
+header=' _____ _      _         _    _           _
+|  __ (_)    | |       | |  | |         | |
+| |__) |  ___| |_ ___  | |__| | ___  ___| |_
+|  ___/ |/ _ \ __/ __| |  __  |/ _ \/ __| __|	+-+-+-+-+
+| |   | |  __/ |_\__ \ | |  | | (_) \__ \ |_ 	| v 1.1 |
+|_|   |_|\___|\__|___/ |_|  |_|\___/|___/\__|	+-+-+-+-+'
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
 
 # Directories - change the following lines to suit your needs:
-HTML=/var/www/html		# root html directory
-BACKUP=$HTML/backup		# name of the backup folder, which will be created
-NCPATH=$HTML/nextcloud1	# name of your subfolder in root html directory, where your nextcloud installation is located
-EMAIL=example@domain.com	# will be used for sending emails, if upgrade was successfull
+html=/var/www/html		# root html directory
+backup=$html/backup		# name of the backup folder, which will be created
+ncpath=$html/nextcloud1	# name of your subfolder in root html directory, where your nextcloud installation is located
+email="example@domain.com"	# will be used for sending emails, if upgrade was successfull
 htuser='apache'  		 # Webserver-User (CentOS: apache, suseLinux: wwwrun, etc..)
 htgroup='apache' 		 # Webserver-Group (CentOS: apache, suseLinux: www, etc...)
-NAME=nextcloud_install_1 # Define a name for your Instance, which will be upgraded
+name=nextcloud_install_1 # Define a name for your Instance, which will be upgraded
 
-# Database Variables - Look in your config.php
-DBSERVER=127.0.0.1		# Database host
-DATABASE=databasename	# Database name
-USER=databaseuser		# Database username
-PASS=S€crEtP@s$			# Database password
+# Database Variables
+dbserver=127.0.0.1		# Database host
+database=databasename	# Database name
+user=databaseuser		# Database username
+pass="S€crEtP@s$"			# Database password
 
 # Variables - Do NOT Change!
-STANDARDPATH=$HTML/nextcloud
-FILE=nextcloud.sql.`date +"%Y%m%d"`
-RESTORE=nextcloud.sql
-HOST=$HOSTNAME
-ocpath=$NCPATH
+standardpath=$html/nextcloud
+file=nextcloud.sql.`date +"%Y%m%d"`
+restore=nextcloud.sql
+host=$hostname
+ocpath=$ncpath
 rootuser='root'
 
-NCREPO="https://download.nextcloud.com/server/releases"
+ncrepo="https://download.nextcloud.com/server/releases"
+
+red='\e[31m'
+green='\e[32m'
+yellow='\e[33m'
+reset='\e[0m'
+redbg='\e[41m'
+lightred='\e[91m'
+blue='\e[34m'
+cyan='\e[36m'
+ugreen='\e[4;32m'
+
+printf $green"$header"$reset
+echo ""
 
 # Versions
-chmod +x $NCPATH/occ
-mkdir -p $HTML/backup
+chmod +x $ncpath/occ
+mkdir -p $html/backup
 mkdir -p /var/log/ncupdater
-CURRENTVERSION=$(sudo -u $htuser php $NCPATH/occ status | grep "versionstring" | awk '{print $3}')
-NCVERSION=$(curl -s -m 900 $NCREPO/ | tac | grep unknown.gif | sed 's/.*"nextcloud-\([^"]*\).zip.sha512".*/\1/;q')
+currentversion=$(sudo -u $htuser php $ncpath/occ status | grep "versionstring" | awk '{print $3}')
+ncversion=$(curl -s -m 900 $ncrepo/ | tac | grep unknown.gif | sed 's/.*"nextcloud-\([^"]*\).zip.sha512".*/\1/;q')
 
 # Must be root
 [[ `id -u` -eq 0 ]] || { echo "Must be root to run script, type: sudo -i"; exit 1; }
 
 # Upgrade Nextcloud
 echo "Checking latest released version on the Nextcloud download server and if it's possible to download..."
-wget -q -T 10 -t 2 $NCREPO/nextcloud-$NCVERSION.tar.bz2 > /dev/null
+wget -q -T 10 -t 2 $ncrepo/nextcloud-$ncversion.tar.bz2 > /dev/null
 if [ $? -eq 0 ]; then
 	echo ""
-    echo -e "\e[4;32mSUCCESS!\e[0m"
-	rm -f $HTML/nextcloud-$NCVERSION.tar.bz2
+    printf $ugreen"SUCCESS!\n"$reset
+	rm -f $html/nextcloud-$ncversion.tar.bz2
 else
     echo
-    echo -e "\e[91mNextcloud $NCVERSION doesn't exist.\e[0m"
-    echo "Please check available versions here: $NCREPO"
+    printf $lightred"Nextcloud $ncversion doesn't exist.\n"$reset
+    echo "Please check available versions here: $ncrepo"
     echo
     exit 1
 fi
@@ -91,138 +104,136 @@ echo -ne '\n'
 
 # Check if new version is larger than current version installed.
 function version_gt() { local v1 v2 IFS=.; read -ra v1 <<< "$1"; read -ra v2 <<< "$2"; printf -v v1 %03d "${v1[@]}"; printf -v v2 %03d "${v2[@]}"; [[ $v1 > $v2 ]]; }
-if version_gt "$NCVERSION" "$CURRENTVERSION"
+if version_gt "$ncversion" "$currentversion"
 then
 	echo ""
-    echo -e "Latest version is: \e[4;32m$NCVERSION\e[0m. Current version is: \e[4;31m$CURRENTVERSION\e[0m."
+    printf "Latest version is: ${ugreen}${ncversion}${reset}. Current version is: ${ugreen}${currentversion}\n"$reset
 	echo ""
-    echo -e "\e[32mNew version available! Upgrade continues...\e[0m"
+    printf $green"New version available! Upgrade continues...\n"$reset
 	sleep 2
 else
 	echo ""
-    echo -e "Latest version is: \e[4;32m$NCVERSION\e[0m. Current version is: \e[4;32m$CURRENTVERSION\e[0m."
+    printf "Latest version is: ${ugreen}${ncversion}${reset}. Current version is: ${ugreen}${currentversion}\n"$reset
 	echo ""
     echo "No need to upgrade, this script will exit..."
-	echo "Your Nextcloud Version $NCVERSION is already up to date - No upgrade needed - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$NAME.log
+	echo "Your Nextcloud Version $ncversion is already up to date - No upgrade needed - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$name.log
 	sleep 2
     exit 0
 fi
 
-echo "Backing up files and upgrading to Nextcloud $NCVERSION in 10 seconds..." 
+echo "Backing up files and upgrading to Nextcloud $ncversion in 10 seconds..." 
 echo "Press CTRL+C to abort."
 
-echo -ne '  |=>                  |   (10%)\r'
+printf "  |=>                  |   (10%%)\r"
 sleep 1
-echo -ne '  |===>                |   (20%)\r'
+printf "  |===>                |   (20%%)\r"
 sleep 1
-echo -ne '  |=====>              |   (30%)\r'
+printf "  |=====>              |   (30%%)\r"
 sleep 1
-echo -ne '  |=======>            |   (40%)\r'
+printf "  |=======>            |   (40%%)\r"
 sleep 1
-echo -ne '  |=========>          |   (50%)\r'
+printf "  |=========>          |   (50%%)\r"
 sleep 1
-echo -ne '  |===========>        |   (60%)\r'
+printf "  |===========>        |   (60%%)\r"
 sleep 1
-echo -ne '  |=============>      |   (70%)\r'
+printf "  |=============>      |   (70%%)\r"
 sleep 1
-echo -ne '  |===============>    |   (80%)\r'
+printf "  |===============>    |   (80%%)\r"
 sleep 1
-echo -ne '  |=================>  |   (90%)\r'
+printf "  |=================>  |   (90%%)\r"
 sleep 1
-echo -ne '  |===================>|   (100%)\r'
-echo -ne '\n'
+printf "  |===================>|   (100%%)\r"
+printf "\n"
 
 # Backup data
 echo ""
-echo -e "\e[33mBacking up data... That may take some time - depending on your Installation!\e[0m"
+printf $yellow"Backing up data... That may take some time - depending on your Installation!\n"$reset
 
-rsync -Aax $NCPATH/config $BACKUP 
-rsync -Aax $NCPATH/themes $BACKUP
-rsync -Aax $NCPATH/apps $BACKUP
+rsync -Aax $ncpath/config $backup 
+rsync -Aax $ncpath/themes $backup
+rsync -Aax $ncpath/apps $backup
 
 rsync_param_data="-Aaxv"
-rsync "$rsync_param_data" $NCPATH/data $BACKUP  |\
-     pv -lep -s $(rsync "$rsync_param_data"n $NCPATH/data $BACKUP  | awk 'NF' | wc -l)
+rsync "$rsync_param_data" $ncpath/data $backup  |\
+     pv -lep -s $(rsync "$rsync_param_data"n $ncpath/data $backup  | awk 'NF' | wc -l)
 	 
 	unalias rm     2> /dev/null
-	rm ${FILE}     2> /dev/null
-	rm ${FILE}.gz  2> /dev/null
+	rm ${file}     2> /dev/null
+	rm ${file}.gz  2> /dev/null
 	sleep 1
 	
 	# Database Backup
 	echo ""
-	echo -e "\e[33mStarting Database Backup...\e[0m"
+	printf $yellow"Starting Database Backup...\n"$reset
 	echo ""
 	sleep 1
 	# use this command for a database server on a separate host:
-	#mysqldump --opt --protocol=TCP --user=${USER} --password=${PASS} --host=${DBSERVER} ${DATABASE} > ${FILE}
+	#mysqldump --opt --protocol=TCP --user=${user} --password=${pass} --host=${dbserver} ${database} > ${file}
 
 	# use this command for a database server on localhost. add other options if need be.
-	mysqldump --opt --user=${USER} --password=${PASS} ${DATABASE} > ${FILE}
+	mysqldump --opt --user=${user} --password=${pass} ${database} > ${file}
 	
-	gzip $FILE
-	echo -e "\e[32m${FILE}.gz was created\e[0m"
+	gzip $file
+	printf $green"${file}.gz was created\n"$reset
 	sleep 1
 	echo ""
 
 if [[ $? > 0 ]]
 then
-    echo -e "\e[31mBackup was not OK.\e[0m Please check $BACKUP and see if the folders are backed up properly"
-	echo "NEXTCLOUD UPDATE FAILED - Backup wasn't successfull - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$NAME.log
+    printf $red"Backup was not OK.${reset} Please check $backup and see if the folders are backed up properly"
+	echo "NEXTCLOUD UPDATE FAILED - Backup wasn't successfull - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$name.log
     exit 1
 else
-    echo -e "\e[4;32m"
-    echo "Backup OK!"
-    echo -e "\e[0m"
+    printf $ugreen"Backup OK!\n"$reset
 fi
 
-echo "Downloading $NCREPO/nextcloud-$NCVERSION.tar.bz2..."
+echo "Downloading $ncrepo/nextcloud-$ncversion.tar.bz2..."
 echo ""
-wget -q -T 10 -t 2 $NCREPO/nextcloud-$NCVERSION.tar.bz2 -P $HTML
+wget -q -T 10 -t 2 $ncrepo/nextcloud-$ncversion.tar.bz2 -P $html
 
-if [ -f $HTML/nextcloud-$NCVERSION.tar.bz2 ]
+if [ -f $html/nextcloud-$ncversion.tar.bz2 ]
 then
-    echo -e "$HTML/nextcloud-$NCVERSION.tar.bz2 \e[32mexists\e[0m"
+    printf "$html/nextcloud-$ncversion.tar.bz2 ${green}exists\n"$reset
 else
     echo "Aborting,something went wrong with the download"
-	echo "NEXTCLOUD UPDATE FAILED - Download couldn't be completed - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$NAME.log
+	echo "NEXTCLOUD UPDATE FAILED - Download couldn't be completed - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$name.log
     exit 1
 fi
 
-if [ -d $BACKUP/config/ ]
+if [ -d $backup/config/ ]
 then
-    echo -e "$BACKUP/config/ \e[32mexists\e[0m"
+    printf "$backup/config/ ${green}exists\n"$reset
 else
-    echo "Something went wrong with backing up your old nextcloud instance, please check in $BACKUP if config/ folder exist."
-	echo "NEXTCLOUD UPDATE FAILED - /config couldn't be backed up - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$NAME.log
+    echo "Something went wrong with backing up your old nextcloud instance, please check in $backup if config/ folder exist."
+	echo "NEXTCLOUD UPDATE FAILED - /config couldn't be backed up - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$name.log
     exit 1
 fi
 
-if [ -d $BACKUP/data/ ]
+if [ -d $backup/data/ ]
 then
-    echo -e "$BACKUP/data/ \e[32mexists\e[0m"
+    printf "$backup/data/ ${green}exists\n"$reset
 else
-    echo "Something went wrong with backing up your old nextcloud instance, please check in $BACKUP if data/ folder exist."
-	echo "NEXTCLOUD UPDATE FAILED - /data couldn't be backed up - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$NAME.log
+    echo "Something went wrong with backing up your old nextcloud instance, please check in $backup if data/ folder exist."
+	echo "NEXTCLOUD UPDATE FAILED - /data couldn't be backed up - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$name.log
     exit 1
 fi
 
-if [ -d $BACKUP/apps/ ]
+if [ -d $backup/apps/ ]
 then
-    echo -e "$BACKUP/apps/ \e[32mexists\e[0m"
+    printf "$backup/apps/ ${green}exists\n"$reset
 else
-    echo "Something went wrong with backing up your old nextcloud instance, please check in $BACKUP if apps/ folder exist."
-	echo "NEXTCLOUD UPDATE FAILED - /apps couldn't be backed up - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$NAME.log
+    echo "Something went wrong with backing up your old nextcloud instance, please check in $backup if apps/ folder exist."
+	echo "NEXTCLOUD UPDATE FAILED - /apps couldn't be backed up - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$name.log
     exit 1
 fi
 
-if [ -d $BACKUP/themes/ ]
+if [ -d $backup/themes/ ]
 then
-    echo -e "$BACKUP/themes/ \e[32mexists\e[0m"
+    printf "$backup/themes/ ${green}exists\n"$reset
     echo 
-	chown -R $htuser:$htgroup $BACKUP
-    echo -e "\e[32mAll files are backed up.\e[0m"
-    sudo -u $htuser php $NCPATH/occ maintenance:mode --on
+	chown -R $htuser:$htgroup $backup
+    printf $green"All files are backed up.\n"$reset
+    sudo -u $htuser php $ncpath/occ maintenance:mode --on
 	echo ""
     echo "Removing old Nextcloud instance in 5 seconds..."
 	
@@ -238,34 +249,34 @@ sleep 1
 echo -ne '  |===================>|   (100%)\r'
 echo -ne '\n'
 	
-    rm -rf $NCPATH
+    rm -rf $ncpath
 	echo ""
-	echo -e "\e[32mAll files removed!\e[0m"
+	printf $green"All files removed!\n"$reset
 	echo ""
 	sleep 1
-	echo -e "\e[33mFiles are being extracted... \e[0m"
+	printf $yellow"Files are being extracted... \n"$reset
 	
-	pv -w 80 $HTML/nextcloud-$NCVERSION.tar.bz2 | tar xjf - -C $HTML
-	mv $STANDARDPATH $NCPATH
+	pv -w 80 $html/nextcloud-$ncversion.tar.bz2 | tar xjf - -C $html
+	mv $standardpath $ncpath
 
-	rm $HTML/nextcloud-$NCVERSION.tar.bz2
+	rm $html/nextcloud-$ncversion.tar.bz2
 	sleep 1
 	
 	echo ""
-	echo -e "\e[32mExtract completed.\e[0m"
+	printf $green"Extract completed.\n"$reset
 	echo ""
 	sleep 1
-	echo -e "\e[33mCopying files back to installation.... That may take a long time - depending on your installation!\e[0m"
+	printf $yellow"Copying files back to installation.... That may take a long time - depending on your installation!\n"$reset
 	echo ""
-    cp -R $BACKUP/themes $NCPATH/
-	cp -R $BACKUP/config $NCPATH/
-	cp -R $BACKUP/data $NCPATH/
+    cp -R $backup/themes $ncpath/
+	cp -R $backup/config $ncpath/
+	cp -R $backup/data $ncpath/
 	
-	echo -e "\e[32mCopying completed.\e[0m"
+	printf $green"Copying completed.\n"$reset
 	echo ""
 	sleep 2
 
-	echo -e "\e[33mSetting file permissions...\e[0m"
+	printf $yellow"Setting file permissions...\n"$reset
 	echo ""
 	
 	# Fix permissions
@@ -278,62 +289,62 @@ echo -ne '\n'
 	
 	chmod +x ${ocpath}/occ
 	sleep 2
-	echo -e "\e[32mFile permissions set successfully!\e[0m"
+	printf $green"File permissions set successfully!\n"$reset
 	echo ""
 	
 	# occ upgrade
-	echo -e "\e[32mStarting Upgrade Process....\e[0m"
+	printf $green"Starting Upgrade Process....\n"$reset
 	sleep 2
 	echo ""
-    sudo -u $htuser php $NCPATH/occ maintenance:mode --off
+    sudo -u $htuser php $ncpath/occ maintenance:mode --off
 	echo ""
-    sudo -u $htuser php $NCPATH/occ upgrade
+    sudo -u $htuser php $ncpath/occ upgrade
 else
-    echo "Something went wrong with backing up your old nextcloud instance, please check in $BACKUP if the folders exist."
-	echo "NEXTCLOUD UPDATE FAILED - /themes couldn't be backed up - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$NAME.log
+    echo "Something went wrong with backing up your old nextcloud instance, please check in $backup if the folders exist."
+	echo "NEXTCLOUD UPDATE FAILED - /themes couldn't be backed up - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$name.log
 	echo ""
-	echo -e "\e[33mRestoring Database...\e[0m"
-	gunzip ${FILE}.gz
-	echo "${FILE}"
-	mv ${FILE} ${RESTORE}
-	mysql --user=${USER} --password=${PASS} ${DATABASE} < ${FILE}
+	printf $yellow"Restoring Database...\n"$reset
+	gunzip ${file}.gz
+	echo "${file}"
+	mv ${file} ${restore}
+	mysql --user=${user} --password=${pass} ${database} < ${file}
 	echo ""
-	echo -e "\e[32mDatabase restored successfully...\e[0m"
+	printf $green"Database restored successfully...\n"$reset
 	echo ""
-	echo "NEXTCLOUD UPDATE FAILED - Database restored successfully - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$NAME.log
+	echo "NEXTCLOUD UPDATE FAILED - Database restored successfully - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$name.log
 	sleep 1
     exit 1
 fi
 
-# Change owner of $BACKUP folder to root
-chown -R root:root $BACKUP
-chown -R $htuser:$htgroup $NCPATH
+# Change owner of $backup folder to root
+chown -R root:root $backup
+chown -R $htuser:$htgroup $ncpath
 
-CURRENTVERSION_after=$(sudo -u $htuser php $NCPATH/occ status | grep "versionstring" | awk '{print $3}')
-if [[ "$NCVERSION" == "$CURRENTVERSION_after" ]]
+currentversion_after=$(sudo -u $htuser php $ncpath/occ status | grep "versionstring" | awk '{print $3}')
+if [[ "$ncversion" == "$currentversion_after" ]]
 then
     echo
-    echo "Latest version is: $NCVERSION. Current version is: $CURRENTVERSION_after."
+    echo "Latest version is: $ncversion. Current version is: $currentversion_after."
 	echo ""
-	echo -e "\e[4;32mUPGRADE SUCCESS!\e[0m"
+	printf $ugreen"UPGRADE SUCCESS!\n"$reset
     echo ""
-    echo "NEXTCLOUD UPDATE success - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$NAME.log
+    echo "NEXTCLOUD UPDATE success - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$name.log
 	
 	# Send E-Mail if successfully updated
 	echo "Hey there!
 
 Your Nextcloud-Update completed successfully!
 	
-Host: $HOST 
-Name: $NAME
-directory: $NCPATH
+Host: $host 
+Name: $name
+directory: $ncpath
 	
 `date +"%d.%m.%Y-%H:%M:%S"`
 
-Thank you for using Piet's Host Nextcloud-Updater!" | mail -s "NEXTCLOUD UPDATE SUCCESS - `date +"%d.%m.%Y"`" $EMAIL
-    sudo -u $htuser php $NCPATH/occ status
+Thank you for using Piet's Host Nextcloud-Updater!" | mail -s "NEXTCLOUD UPDATE SUCCESS - `date +"%d.%m.%Y"`" $email
+    sudo -u $htuser php $ncpath/occ status
 	echo ""
-    sudo -u $htuser php $NCPATH/occ maintenance:mode --off
+    sudo -u $htuser php $ncpath/occ maintenance:mode --off
     echo
     echo "Thank you for using Piet's Host Nextcloud-Updater!"
 	echo ""
@@ -342,12 +353,12 @@ Thank you for using Piet's Host Nextcloud-Updater!" | mail -s "NEXTCLOUD UPDATE 
     exit 0
 else
     echo
-	echo echo -e "\e[40;38;4;82mLatest: $NCVERSION \e[30;48;4;82mCurrent: $CURRENTVERSION_after \e[0m"
-    sudo -u $htuser php $NCPATH/occ status
-    echo -e "\e[31mUPGRADE FAILED!\e[0m"
-    echo "Your files are still backed up at $BACKUP. No worries!"
+	printf "\e[40;38;4;82mLatest: $ncversion \e[30;48;4;82mCurrent: $currentversion_after \n"$reset
+    sudo -u $htuser php $ncpath/occ status
+    printf $red"UPGRADE FAILED!\n"$reset
+    echo "Your files are still backed up at $backup. No worries!"
     echo "Please report this issue to support@piets-host.de"
 	echo ""
-	echo "NEXTCLOUD UPDATE FAILED - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$NAME.log
+	echo "NEXTCLOUD UPDATE FAILED - `date +"%Y_%m_%d"`" >> /var/log/ncupdater/ncupdater_$name.log
     exit 1
 fi
